18627f686305f1d8f7230e3b642c1e42eb0f802a
romarSanJuan <sanjuanromar15@gmail.com>
2017-01-15 09:10:18 +0800

    sample only

---
+++ model/blobs.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');  
+var blobSchema = new mongoose.Schema({  
+  name: String,
+  locale: String,
+  president: String,
+  established: Number,
+  population: Number,
+  website: String,
+  social: String,
+  image: String,
+  dob: { type: Date, default: Date.now },
+});
+mongoose.model('Blob', blobSchema);
---
+++ model/db.js
@@ -0,0 +1,2 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://diwdiiiw:diwtrick1221@ds159998.mlab.com:59998/coen3464-t13');
